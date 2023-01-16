8937ba1bb59eca2545dfeeb8d275321a2043b591
Aneebch <aneeb.chatha786@gmail.com>
2023-01-16 14:40:40 +0100

    test 2

--- __test__/retos01.test.js
+++ __test__/retos01.test.js
@@ -1,5 +1,6 @@
 const { maxNumber, sumaPares } = require('../src/retos01');
 
+
 test('debe devolver el número mayor de una array de números', () => {
   //GIVEN
   let numbersList = [1, 2, -5, 0];
@@ -9,3 +10,14 @@ test('debe devolver el número mayor de una array de números', () => {
   //THEN
   expect(sut).toBe(2);
 });
+
+
+test('debe devolver la suma de los numeros pares', () => {
+  //GIVEN
+  let oddList = [1, 2, 5, 0, 4, 3, 6];
+  //WHEN
+  let sut = sumaPares(oddList);
+  //THEN
+  expect(sut).toBe(12);
+});
+
