04f737481fc6647896a94904ed3ed82dbd51502c
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-01-16 14:48:10 +0100

    made sumaPares function for second test

--- src/retos01.js
+++ src/retos01.js
@@ -8,5 +8,14 @@ let maxNumber = (array) => {
   return max;
 };
 
-let sumaPares = () => {};
+let sumaPares = (array) => {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 == 0) {
+      sum += array[i];
+    }
+    
+  }
+  return sum;
+};
 module.exports = { maxNumber, sumaPares };
